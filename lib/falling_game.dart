8257d8f3bbb1dd4233ae3a6fa4e8dad696ee7385
heitorlupino <159820439+heitorlupino@users.noreply.github.com>
2025-04-28 15:42:05 -0300

    update tela jogo roupas

--- lib/falling_game.dart
+++ lib/falling_game.dart
@@ -2,13 +2,16 @@ import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+// Modelo do objeto que cai
 class FallingObject {
   final double left;
   final int id;
+  final String imagePath;
 
-  FallingObject({required this.left, required this.id});
+  FallingObject({required this.left, required this.id, required this.imagePath});
 }
 
+// Tela principal do mini game
 class GameScreen extends StatefulWidget {
   const GameScreen({Key? key}) : super(key: key);
 
@@ -21,10 +24,16 @@ class _GameScreenState extends State<GameScreen> {
   final Random random = Random();
   int _objectId = 0;
 
+  // Lista de imagens possíveis
+  final List<String> objectImages = [
+    'assets/images/jogoRoupas/Calça_marrom_boa_transparente.png',
+    'assets/images/jogoRoupas/Calça_azulClaro_boa.png',
+  ];
+
   @override
   void initState() {
     super.initState();
-    Timer.periodic(Duration(seconds: 1), (timer) {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
       _addFallingObject();
     });
   }
@@ -32,8 +41,9 @@ class _GameScreenState extends State<GameScreen> {
   void _addFallingObject() {
     setState(() {
       objects.add(FallingObject(
-        left: random.nextDouble() * 300,
+        left: random.nextDouble() * 300, // posição horizontal aleatória
         id: _objectId++,
+        imagePath: objectImages[random.nextInt(objectImages.length)], // imagem aleatória
       ));
     });
   }
@@ -41,28 +51,44 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: objects.map((obj) {
-          return FallingWidget(
-            key: ValueKey(obj.id),
-            left: obj.left,
-            onEnd: () {
-              setState(() {
-                objects.removeWhere((o) => o.id == obj.id);
-              });
-            },
-          );
-        }).toList(),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/jogoRoupas/fundo_jogoRoupas.jpeg'), // Fundo do jogo
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: objects.map((obj) {
+            return FallingWidget(
+              key: ValueKey(obj.id),
+              left: obj.left,
+              imagePath: obj.imagePath,
+              onEnd: () {
+                setState(() {
+                  objects.removeWhere((o) => o.id == obj.id);
+                });
+              },
+            );
+          }).toList(),
+        ),
       ),
     );
   }
 }
 
+// Widget da imagem caindo
 class FallingWidget extends StatefulWidget {
   final double left;
+  final String imagePath;
   final VoidCallback onEnd;
 
-  const FallingWidget({Key? key, required this.left, required this.onEnd}) : super(key: key);
+  const FallingWidget({
+    Key? key,
+    required this.left,
+    required this.imagePath,
+    required this.onEnd,
+  }) : super(key: key);
 
   @override
   _FallingWidgetState createState() => _FallingWidgetState();
@@ -75,7 +101,10 @@ class _FallingWidgetState extends State<FallingWidget> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(duration: Duration(seconds: 3), vsync: this);
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3),
+      vsync: this,
+    );
     _topAnimation = Tween(begin: -50.0, end: 800.0).animate(_controller)
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
@@ -99,7 +128,11 @@ class _FallingWidgetState extends State<FallingWidget> with SingleTickerProvider
         return Positioned(
           top: _topAnimation.value,
           left: widget.left,
-          child: Icon(Icons.star, size: 40, color: Colors.amber),
+          child: Image.asset(
+            widget.imagePath,
+            width: 50,
+            height: 50,
+          ),
         );
       },
     );
